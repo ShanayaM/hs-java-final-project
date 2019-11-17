70affbc8bcf0ee3ea38c3e3a3ebad9aa7f94585b
ShanayaM <msdec.ict@gmail.com>
2019-11-17 13:59:44 +0100

    Update Song.java

--- src/main/project/Song.java
+++ src/main/project/Song.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package project;
 
 import java.util.HashMap;
 import java.util.Map;
 import prj5.SongInterface;
 
-/**
- * 
- * @author parshad
- */
 public class Song implements SongInterface{
     private String title;
     private String artist;
