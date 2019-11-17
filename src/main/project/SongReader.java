ee3f59b7f8519c82f15be97c6f41f4d7701f8e8f
pbarot10 <44982194+pbarot10@users.noreply.github.com>
2019-11-17 14:05:14 +0100

    Create SongReader.java

---
+++ src/main/project/SongReader.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import project.SongInterface;
+
+/**
+ *
+ * 
+ */
+public class SongReader {
+
+    public static List getSongList() {
+        String csvFile = System.getProperty("user.dir") + File.separator
+            + "SongList2018.csv";
+        System.out.println("csvFile = " + csvFile);
+
+        List songList = new ArrayList();
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+        try {
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+                // use comma as separator
+                String[] data = line.split(cvsSplitBy);
+                String title = data[0];
+                String artist = data[1];
+                String year = data[2];
+                String genre = data[3];
+                if (isValid(title) && isValid(artist) && isValid(year)
+                    && isValid(genre)) {
+                    SongInterface song = new Song();
+                    song.setTitle(title);
+                    song.setArtist(artist);
+                    song.setYear(year);
+                    song.setGenre(genre);
+                    song.setArtist(artist);
+                    songList.add(song);
+                }
+            }
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (br != null) {
+                try {
+                    br.close();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return songList;
+    }
+
+
+    // A filter to remove help us skip first row of csv file
+    public static boolean isValid(String data) {
+        boolean isValid = true;
+        if ("Song Title".equals(data)) {
+            isValid = false;
+        }
+        if ("Artist".equals(data)) {
+            isValid = false;
+        }
+        if ("Year".equals(data)) {
+            isValid = false;
+        }
+        if ("Genre".equals(data)) {
+            isValid = false;
+        }
+        return isValid;
+    }
+}
