d6627da21b79575d8a192d1bcf88f2923de066eb
pbarot10 <pbarot10@vt.edu>
2019-11-20 18:42:46 +0100

    added GUIFunction and GUIGlyph

---
+++ src/main/project/GUIFunction.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+
+import project.MusicSurveyInterface.SurveyEnum;
+import java.util.List;
+import project.GUIInterface;
+
+/**
+ *
+ *
+ */
+public class GUIFunction {
+    public GUIFunction(List model, GUIInterface view){
+        //Controller keeps model and view separate from each other.
+
+        //Update the view with the model
+        view.updateModel(model, 0, SurveyEnum.HOBBY);
+
+        //NORTH BUTTONS
+        view.getPreviousButton().onClick(new GUIPreviousButton(model, view));
+        view.getSortByArtistNameButton().onClick(new GUISortByArtistNameButton(model, view));
+        view.getSortBySongTitleButton().onClick(new GUISortBySongTitleButton(model, view));
+        view.getSortByReleaseYearButton().onClick(new GUISortByReleaseYearButton(model, view));
+        view.getSortByGenreButton().onClick(new GUISortByGenreButton(model, view));
+        view.getNextButton().onClick(new GUINextButton(model, view));
+        //SOUTH BUTTONS
+        view.getRepresentHobbyButton().onClick(new GUIRepresentHobyyButton(model, view));
+        view.getRepresentMajorButton().onClick(new GUIRepresentMajorButton(model, view));
+        view.getRepresentRegionButton().onClick(new GUIRepresentRegionButton(model, view));
+        view.getQuitButton().onClick(new GUIQuitButton(model, view));
+    }
+}
