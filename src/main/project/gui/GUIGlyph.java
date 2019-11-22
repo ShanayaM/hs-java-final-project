fe4d5182fd61b2d28e1cd61270f733873c7d107f
ShanayaM <msdec.ict@gmail.com>
2019-11-22 14:32:16 +0100

    Moved to package gui

--- src/main/project/gui/GUIGlyph.java
+++ src/main/project/gui/GUIGlyph.java
@@ -1,9 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 
+ * Glyph GUI
+ * 
  */
-package project;
+package gui.project;
 
 import CS2114.Shape;
 import CS2114.TextShape;
