80f819899a4e25ff1bb7b4c2db3d4f67d3d59b08
ShanayaM <msdec.ict@gmail.com>
2019-11-22 15:36:12 +0100

    Update GUIQuitButton.java

--- src/main/project/GUIQuitButton.java
+++ src/main/project/GUIQuitButton.java
@@ -1,24 +1,22 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 
+ * GUI for rendering quit button
+ * 
  */
 package project;
 
 import CS2114.Command;
 import CS2114.Widget;
 import java.util.List;
-import project.GUIInterface;
 
 /**
  *
  */
-public class GUIQuitButton extends GUIModel implements Command {
+public final class GUIQuitButton extends GUIModel implements Command {
 
-    public GUIQuitButton(List model, GUIInterface view) {
+    public GUIQuitButton(final List model, final GUIInterface view) {
         super(model, view);
     }
-
 
     @Override
     public void execute(Widget widget) {
