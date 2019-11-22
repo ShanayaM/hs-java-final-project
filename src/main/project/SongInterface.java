0037c38f9b0d47d2c436c3b1ccf7bf16a7eb685b
ShanayaM <msdec.ict@gmail.com>
2019-11-22 15:55:50 +0100

    Update SongInterface.java

--- src/main/project/SongInterface.java
+++ src/main/project/SongInterface.java
@@ -10,32 +10,14 @@ package project;
  * 
  */
 public interface SongInterface {
-    public String getTitle();
-
-
-    public void setTitle(String title);
-
-
-    public String getArtist();
-
-
-    public void setArtist(String artist);
-
-
-    public String getYear();
-
-
-    public void setYear(String year);
-
-
-    public String getGenre();
-
-
-    public void setGenre(String genre);
-
-
-    public void increment(String key);
-
-
-    public int getTotal(String key);
+    String getTitle();
+    void setTitle(String title);
+    String getArtist();
+    void setArtist(String artist);
+    String getYear();
+    void setYear(String year);
+    String getGenre();
+    void setGenre(String genre);
+    void increment(String key);
+    int getTotal(String key);
 }
