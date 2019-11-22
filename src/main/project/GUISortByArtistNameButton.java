b73c53ecbe50189b6610c3e7ebf26d6879c9cf44
ShanayaM <msdec.ict@gmail.com>
2019-11-22 15:39:56 +0100

    Update GUISortByArtistNameButton.java

--- src/main/project/GUISortByArtistNameButton.java
+++ src/main/project/GUISortByArtistNameButton.java
@@ -9,26 +9,26 @@ import CS2114.Command;
 import CS2114.Widget;
 import java.util.Comparator;
 import java.util.List;
-import project.SongInterface;
-import project.GUIInterface;
 
 /**
  *
  */
-public class GUISortByArtistNameButton extends GUIModel implements Command{
+public final class GUISortByArtistNameButton
+    extends GUIModel implements Command {
 
-    public GUISortByArtistNameButton(List model, GUIInterface view) {
+    public GUISortByArtistNameButton(final List model,
+                                     final GUIInterface view) {
         super(model, view);
     }
 
     @Override
-    public void execute(Widget widget) {
+    public void execute(final Widget widget) {
         model.sort(COMPARE_BY_ARTIST_NAME);
         view.updateModel(model, 0, view.getCurrentType());
     }
 
     public static Comparator<SongInterface> COMPARE_BY_ARTIST_NAME = new Comparator<SongInterface>() {
-        public int compare(SongInterface one, SongInterface other) {
+        public int compare(final SongInterface one, final SongInterface other) {
             return one.getArtist().toLowerCase().compareTo(other.getArtist().toLowerCase());
         }
     };
