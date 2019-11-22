ba60de11b813f0d48a3f7db3d9ee164e1fa86157
ShanayaM <msdec.ict@gmail.com>
2019-11-22 16:00:55 +0100

    Update GUIFunction.java

--- src/main/project/GUIFunction.java
+++ src/main/project/GUIFunction.java
@@ -7,14 +7,14 @@ package project;
 
 import project.MusicSurveyInterface.SurveyEnum;
 import java.util.List;
-import project.GUIInterface;
 
 /**
  *
  *
  */
-public class GUIFunction {
-    public GUIFunction(final List model, final GUIInterface view){
+public final class GUIFunction {
+    public GUIFunction(final List model,
+                       final GUIInterface view) {
         //Controller keeps model and view separate from each other.
 
         //Update the view with the model
@@ -22,15 +22,24 @@ public class GUIFunction {
 
         //NORTH BUTTONS
         view.getPreviousButton().onClick(new GUIPreviousButton(model, view));
-        view.getSortByArtistNameButton().onClick(new GUISortByArtistNameButton(model, view));
-        view.getSortBySongTitleButton().onClick(new GUISortBySongTitleButton(model, view));
-        view.getSortByReleaseYearButton().onClick(new GUISortByReleaseYearButton(model, view));
-        view.getSortByGenreButton().onClick(new GUISortByGenreButton(model, view));
-        view.getNextButton().onClick(new GUINextButton(model, view));
+        view.getSortByArtistNameButton()
+            .onClick(new GUISortByArtistNameButton(model, view));
+        view.getSortBySongTitleButton()
+            .onClick(new GUISortBySongTitleButton(model, view));
+        view.getSortByReleaseYearButton()
+            .onClick(new GUISortByReleaseYearButton(model, view));
+        view.getSortByGenreButton()
+            .onClick(new GUISortByGenreButton(model, view));
+        view.getNextButton()
+            .onClick(new GUINextButton(model, view));
         //SOUTH BUTTONS
-        view.getRepresentHobbyButton().onClick(new GUIRepresentHobyyButton(model, view));
-        view.getRepresentMajorButton().onClick(new GUIRepresentMajorButton(model, view));
-        view.getRepresentRegionButton().onClick(new GUIRepresentRegionButton(model, view));
-        view.getQuitButton().onClick(new GUIQuitButton(model, view));
+        view.getRepresentHobbyButton()
+            .onClick(new GUIRepresentHobyyButton(model, view));
+        view.getRepresentMajorButton()
+            .onClick(new GUIRepresentMajorButton(model, view));
+        view.getRepresentRegionButton()
+            .onClick(new GUIRepresentRegionButton(model, view));
+        view.getQuitButton()
+            .onClick(new GUIQuitButton(model, view));
     }
 }
