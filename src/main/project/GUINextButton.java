0d8a0e07e133171ad8775e194450f9bdd07a81a9
ShanayaM <msdec.ict@gmail.com>
2019-11-22 15:34:05 +0100

    Update GUINextButton.java

--- src/main/project/GUINextButton.java
+++ src/main/project/GUINextButton.java
@@ -1,30 +1,29 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 
+ * GUI for rendering next button
+ * 
  */
 package project;
 
 import CS2114.Command;
 import CS2114.Widget;
 import java.util.List;
-import project.GUIInterface;
 
 /**
  *
  *
  */
-public class GUINextButton extends GUIModel implements Command{
+public class GUINextButton extends GUIModel implements Command {
 
-    public GUINextButton(List model, GUIInterface view) {
+    public GUINextButton(final List model, final GUIInterface view) {
         super(model, view);
     }
 
     @Override
     public void execute(Widget widget) {
         int currentIndex = view.getPaginationIndex();
-        int nextIndex = currentIndex+1;
-        if(nextIndex<model.size()){
+        int nextIndex = currentIndex + 1;
+        if (nextIndex < model.size()) {
             view.getNextButton().enable();
             view.getPreviousButton().enable();
             view.updateModel(model, nextIndex, view.getCurrentType());
